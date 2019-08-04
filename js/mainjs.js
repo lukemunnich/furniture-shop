1066c30c27b98e6d71637062da5b73ba0b20b8c1
luke munnich <lukemunnich@gmail.com>
2019-08-04 12:02:00 +0200

    added lookbook images and css

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,8 +1,16 @@
-function myFunction() {
+function viewMore() {
     var x = document.getElementById("myDIV");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
+  }
+
+  function imageGallery(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
   }
