117ef088ab9cf96b1b2a8ec350222d9160e6d246
luke munnich <lukemunnich@gmail.com>
2019-08-05 13:32:32 +0200

    added add to cart button

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,35 +1,35 @@
-// function viewMore() {
-//     var x = document.getElementById("myDIV");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "block";
-//     }
-//   }
+function viewMore() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
 
-//   function imageGallery(imgs) {
-//     var expandImg = document.getElementById("expandedImg");
-//     var imgText = document.getElementById("imgtext");
-//     expandImg.src = imgs.src;
-//     imgText.innerHTML = imgs.alt;
-//     expandImg.parentElement.style.display = "block";
-//   }
+  function imageGallery(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
 
-//   function imageGalleries(imgs) {
-//     var expandImg = document.getElementById("expandedSecond");
-//     var imgText = document.getElementById("imgtext");
-//     expandImg.src = imgs.src;
-//     imgText.innerHTML = imgs.alt;
-//     expandImg.parentElement.style.display = "block";
-//   }
+  function imageGalleries(imgs) {
+    var expandImg = document.getElementById("expandedSecond");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
 
-//   function imageGalleryThird(imgs) {
-//     var expandImg = document.getElementById("expandedThird");
-//     var imgText = document.getElementById("imgtext");
-//     expandImg.src = imgs.src;
-//     imgText.innerHTML = imgs.alt;
-//     expandImg.parentElement.style.display = "block";
-//   }
+  function imageGalleryThird(imgs) {
+    var expandImg = document.getElementById("expandedThird");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
 
 
   // shoppign cart
